8282095df755bc6fdbbcfbd76a7ceb87ebf43621
Dmytro Martyshchuk <Di.mart.ap@gmail.com>
2023-11-16 16:02:05 +0200

    Initial commit

---
+++ src/main/java/com/app/botblend/BotblendApplication.java
@@ -0,0 +1,13 @@
+package com.app.botblend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotblendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BotblendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/botblend/BotblendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.botblend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotblendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
