c491a9e7d14216c8a393e4b6d56324471654c7b7
Dmytro Martyshchuk <Di.mart.ap@gmail.com>
2023-11-20 17:33:43 +0200

    Fix openai message truncate (#12)

--- src/main/java/com/app/botblend/client/openai/OpenAiClient.java
+++ src/main/java/com/app/botblend/client/openai/OpenAiClient.java
@@ -5,8 +5,9 @@ import com.app.botblend.client.openai.model.CompletionResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@FeignClient(value = "chatGpt", url = "${openai.api.baseurl}")
+@FeignClient(value = "open-ai", url = "${openai.api.baseurl}")
 public interface OpenAiClient {
+
     @PostMapping(path = "${openai.api.path.completions}")
     CompletionResponse createChatCompletion(CompletionRequest requestDto);
 }
